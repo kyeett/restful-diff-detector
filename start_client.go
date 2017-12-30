d2b5d5f11dae6ab282af3ac04ebaa1ba270872e7
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2017-12-30 22:15:23 +0100

    Add start scripts for client and server

---
+++ start_client.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kyeett/restful-diff-detector/grpc_client"
+)
+
+func main() {
+	grpcclient.MakeFlow()
+}
---
+++ start_server.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kyeett/restful-diff-detector/grpc_server"
+)
+
+func main() {
+	grpcserver.ServerMain()
+}
